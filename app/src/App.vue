20d718e20e30eaa1246209e0844e0c0abac0f172
Your Name <you@example.com>
2019-11-07 16:24:36 +0800

    初始框架

---
+++ app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <p class="h2">16px</p>
+    <p class="h3">24px</p>
+    <p class="h4">甜品36px</p>
+    <router-view />
+    <vue1></vue1>
+  </div>
+</template>
+
+<script>
+import vue1 from "./components/vue1";
+export default {
+  components: {
+    vue1
+  },
+  created() {
+    window.onload = function() {
+      document.documentElement.style.fontSize =
+        (document.documentElement.clientWidth / 1080) * 100 + "px";
+      window.onresize = function() {
+        document.documentElement.style.fontSize =
+          (document.documentElement.clientWidth / 1080) * 100 + "px";
+      };
+    };
+  }
+};
+</script>
+
+<style>
+.h2 {
+  font-size: 0.16rem;
+}
+.h3 {
+  font-size: 0.24rem;
+}
+.h4 {
+  font-size: 0.36rem;
+}
+</style>
+
---
+++ app/src/components/vue1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="vue1">
+    <p>36px->0.36rem</p>p
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#vue1 p {
+  font-size: 0.36rem;
+}
+</style>
